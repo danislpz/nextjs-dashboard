c9316b49a6fab1cfc269350b6530ebd4bfd0fcde
danislpz <94924957+danislpz@users.noreply.github.com>
2024-07-08 08:49:23 -0500

    Fetch Revenue and Latest Invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,8 +18,9 @@ export async function fetchRevenue() {
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
+    
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -30,6 +31,12 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // const data = await sql<LatestInvoiceRaw>`
+    // SELECT invoices.amount, customers.name, customers.image_url, customers.email
+    // FROM invoices
+    // JOIN customers ON invoices.customer_id = customers.id
+    // ORDER BY invoices.date DESC
+    // LIMIT 5`;
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
